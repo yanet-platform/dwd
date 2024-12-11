c6cbc5ec43925c99eff47242af95eb9e7155b645
Evgenyi Safronov <division494@gmail.com>
2024-12-11 14:00:54 +0300

    refactor: clean up dpdk bindings, format

--- dpdk/src/ffi/stub.c
+++ dpdk/src/ffi/stub.c
@@ -1,3 +1,5 @@
+//! Here we derive functions that expose macro and inline functions.
+
 #define _GNU_SOURCE
 #include <stdio.h>
 
@@ -7,40 +9,59 @@
 #include <rte_log.h>
 #include <rte_mbuf.h>
 
-int rte_errno_() {
+int
+_rte_errno() {
     return rte_errno;
 }
 
-struct rte_mbuf* rte_pktmbuf_alloc_(struct rte_mempool *mp) {
+struct rte_mbuf*
+_rte_pktmbuf_alloc(struct rte_mempool* mp) {
     return rte_pktmbuf_alloc(mp);
 }
 
-char* rte_pktmbuf_append_(struct rte_mbuf* mbuf, uint16_t len) {
+char*
+_rte_pktmbuf_append(struct rte_mbuf* mbuf, uint16_t len) {
     return rte_pktmbuf_append(mbuf, len);
 }
 
-unsigned rte_lcore_id_stub() {
+unsigned
+_rte_lcore_id() {
     return rte_lcore_id();
 }
 
-uint16_t rte_eth_rx_burst_stub(uint16_t port_id, uint16_t queue_id, struct rte_mbuf **rx_pkts, uint16_t nb_pkts) {
+uint16_t
+_rte_eth_rx_burst(
+    uint16_t port_id,
+    uint16_t queue_id,
+    struct rte_mbuf** rx_pkts,
+    uint16_t nb_pkts
+) {
     return rte_eth_rx_burst(port_id, queue_id, rx_pkts, nb_pkts);
 }
 
-uint16_t rte_eth_tx_burst_stub(uint16_t port_id, uint16_t queue_id, struct rte_mbuf **tx_pkts, uint16_t nb_pkts) {
+uint16_t
+_rte_eth_tx_burst(
+    uint16_t port_id,
+    uint16_t queue_id,
+    struct rte_mbuf** tx_pkts,
+    uint16_t nb_pkts
+) {
     return rte_eth_tx_burst(port_id, queue_id, tx_pkts, nb_pkts);
 }
 
-void rte_mbuf_refcnt_set_stub(struct rte_mbuf *m, uint16_t new_value) {
+void
+_rte_mbuf_refcnt_set(struct rte_mbuf* m, uint16_t new_value) {
     rte_mbuf_refcnt_set(m, new_value);
 }
 
-static ssize_t rte_xx_memfile_write(void* handler, const char* buf, size_t size) {
-    ((void(*)(const char*, size_t))handler)(buf, size);
+static ssize_t
+rte_xx_memfile_write(void* handler, const char* buf, size_t size) {
+    ((void (*)(const char*, size_t))handler)(buf, size);
     return size;
 }
 
-void* rte_xx_init_logging(void* log) {
+void*
+rte_xx_init_logging(void* log) {
     void* fh = NULL;
 
     cookie_io_functions_t io;
@@ -56,7 +77,8 @@ void* rte_xx_init_logging(void* log) {
     return fh;
 }
 
-void rte_xx_free_logging(void* fh) {
+void
+rte_xx_free_logging(void* fh) {
     rte_openlog_stream(NULL);
     fclose(fh);
 }
--- dpdk/src/lib.rs
+++ dpdk/src/lib.rs
@@ -23,25 +23,20 @@ pub mod ffi {
 
 #[link(name = "stub")]
 extern "C" {
+    fn _rte_errno() -> core::ffi::c_int;
+    
+    fn _rte_pktmbuf_alloc(mp: *mut ffi::rte_mempool) -> *mut ffi::rte_mbuf;
+    fn _rte_pktmbuf_append(mbuf: *mut ffi::rte_mbuf, len: core::ffi::c_ushort) -> *mut core::ffi::c_char;
     // fn rte_pktmbuf_free_(packet: *mut rte_mbuf);
-    fn rte_pktmbuf_alloc_(mp: *mut ffi::rte_mempool) -> *mut ffi::rte_mbuf;
-    fn rte_pktmbuf_append_(mbuf: *mut ffi::rte_mbuf, len: core::ffi::c_ushort) -> *mut core::ffi::c_char;
-    fn rte_eth_tx_burst_stub(port_id: u16, queue_id: u16, tx_pkts: *mut *mut ffi::rte_mbuf, nb_pkts: u16) -> u16;
-    fn rte_eth_rx_burst_stub(port_id: u16, queue_id: u16, rx_pkts: *mut *mut ffi::rte_mbuf, nb_pkts: u16) -> u16;
-    // fn rte_mbuf_refcnt_read_(m: *const rte_mbuf) -> u16;
-    // fn rte_mbuf_refcnt_update_(m: *mut rte_mbuf, value: i16) -> u16;
-    // fn rte_pktmbuf_adj_(packet: *mut rte_mbuf, len: u16) -> *mut c_char;
-    // fn rte_pktmbuf_trim_(packet: *mut rte_mbuf, len: u16) -> c_int;
-    // fn rte_pktmbuf_headroom_(m: *const rte_mbuf) -> u16;
-    // fn rte_pktmbuf_tailroom_(m: *const rte_mbuf) -> u16;
-    fn rte_errno_() -> core::ffi::c_int;
-    // fn rte_pktmbuf_chain_(head: *mut rte_mbuf, tail: *mut rte_mbuf) -> c_int;
-    fn rte_lcore_id_stub() -> core::ffi::c_uint;
-    fn rte_mbuf_refcnt_set_stub(mbuf: *mut ffi::rte_mbuf, new_value: u16);
+    fn _rte_eth_tx_burst(port_id: u16, queue_id: u16, tx_pkts: *mut *mut ffi::rte_mbuf, nb_pkts: u16) -> u16;
+    fn _rte_eth_rx_burst(port_id: u16, queue_id: u16, rx_pkts: *mut *mut ffi::rte_mbuf, nb_pkts: u16) -> u16;
+    fn _rte_lcore_id() -> core::ffi::c_uint;
+    fn _rte_mbuf_refcnt_set(mbuf: *mut ffi::rte_mbuf, new_value: u16);
     fn rte_xx_init_logging(data: extern "C" fn(buf: *const core::ffi::c_char, len: usize)) -> *mut core::ffi::c_void;
     fn rte_xx_free_logging(fh: *mut core::ffi::c_void);
 }
 
+#[inline]
 pub fn rte_lcore_to_socket_id(id: CoreId) -> SocketId {
     unsafe { ffi::rte_lcore_to_socket_id(id.as_u16() as SocketId) }
 }
@@ -53,36 +48,36 @@ pub fn rte_lcore_to_socket_id(id: CoreId) -> SocketId {
 /// of additional possible values for RTE-specific conditions.
 #[inline]
 pub unsafe fn rte_errno() -> core::ffi::c_int {
-    rte_errno_()
+    _rte_errno()
 }
 
 #[inline]
 pub unsafe fn rte_pktmbuf_alloc(mp: *mut ffi::rte_mempool) -> *mut ffi::rte_mbuf {
-    rte_pktmbuf_alloc_(mp)
+    _rte_pktmbuf_alloc(mp)
 }
 
 #[inline]
 pub unsafe fn rte_pktmbuf_append(mbuf: *mut ffi::rte_mbuf, len: core::ffi::c_ushort) -> *mut core::ffi::c_char {
-    rte_pktmbuf_append_(mbuf, len)
-}
-
-#[inline]
-pub unsafe fn rte_eth_rx_burst(port_id: u16, queue_id: u16, rx_pkts: *mut *mut ffi::rte_mbuf, nb_pkts: u16) -> u16 {
-    rte_eth_rx_burst_stub(port_id, queue_id, rx_pkts, nb_pkts)
+    _rte_pktmbuf_append(mbuf, len)
 }
 
 #[inline]
 pub unsafe fn rte_eth_tx_burst(port_id: u16, queue_id: u16, tx_pkts: *mut *mut ffi::rte_mbuf, nb_pkts: u16) -> u16 {
-    rte_eth_tx_burst_stub(port_id, queue_id, tx_pkts, nb_pkts)
+    _rte_eth_tx_burst(port_id, queue_id, tx_pkts, nb_pkts)
+}
+
+#[inline]
+pub unsafe fn rte_eth_rx_burst(port_id: u16, queue_id: u16, rx_pkts: *mut *mut ffi::rte_mbuf, nb_pkts: u16) -> u16 {
+    _rte_eth_rx_burst(port_id, queue_id, rx_pkts, nb_pkts)
 }
 
 #[inline]
 pub fn rte_lcore_id() -> CoreId {
-    let id = unsafe { rte_lcore_id_stub() } as u16;
+    let id = unsafe { _rte_lcore_id() } as u16;
     CoreId::new(id)
 }
 
 #[inline]
 pub unsafe fn rte_mbuf_refcnt_set(mbuf: *mut ffi::rte_mbuf, new_value: u16) {
-    rte_mbuf_refcnt_set_stub(mbuf, new_value);
+    _rte_mbuf_refcnt_set(mbuf, new_value);
 }
