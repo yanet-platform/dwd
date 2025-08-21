d83433e455e4d9c8029092a31bb925b708ffe2c0
Evgeny Safronov <division494@gmail.com>
2025-08-21 14:11:21 +0300

    refactor: send logs to stderr (#20)

--- dwd/src/logging.rs
+++ dwd/src/logging.rs
@@ -1,22 +1,23 @@
-use std::error::Error;
+use std::{error::Error, io::stderr};
 
-use log::LevelFilter;
-use simple_logger::SimpleLogger;
+use tracing::level_filters::LevelFilter;
+use tracing_subscriber::{filter::Targets, layer::SubscriberExt, util::SubscriberInitExt};
 
 pub fn init(verbosity: usize) -> Result<(), Box<dyn Error>> {
     let level = match verbosity {
-        0 => LevelFilter::Info,
-        1 => LevelFilter::Debug,
-        _ => LevelFilter::Trace,
+        0 => LevelFilter::INFO,
+        1 => LevelFilter::DEBUG,
+        _ => LevelFilter::TRACE,
     };
 
-    SimpleLogger::new()
-        .with_level(LevelFilter::Off)
-        .with_module_level("dwd", level)
-        .with_module_level("actix_web", level)
-        .with_module_level("dpdk", level)
-        .with_utc_timestamps()
-        .init()?;
+    let filter_layer = Targets::new()
+        .with_target("dwd", level)
+        .with_target("dpdk", level)
+        .with_default(LevelFilter::WARN.min(level));
+    let fmt_layer = tracing_subscriber::fmt::layer().with_writer(stderr);
+    let registry = tracing_subscriber::registry().with(filter_layer).with(fmt_layer);
+
+    registry.try_init()?;
 
     Ok(())
 }
